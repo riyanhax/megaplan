1ba310a8432f29bd68f868e8243daec0d79535d3
Влад Балакин <vladbalakin@gmail.com>
2017-06-22 09:53:09 +0500

    com

--- local/templates/megaplan/js/prices.js
+++ local/templates/megaplan/js/prices.js
@@ -11,16 +11,14 @@ $(document).ready(function () {
     var contentHeight = $(".content").height();
     var popupHeight = $("#compare_plans").height();
     var topPopupHeight = popupHeight - popupHeight * 2;
-    console.log(isPopupOpen);
     $("#compare_plans .close_popup a").click(function () {
         $("#compare_plans").animate({
-            top: "-" + popupHeight + "px",
+            top: "-100%",
         }, {
             duration: 600
         });
         isPopupOpen = 0;
         console.log(isPopupOpen);
-        $("#compare_plans").css("top",topPopupHeight);
         $(".content>div").css("display","block").css("visibility","visible");
         $(".content").height("initial").css("background-image","url(/local/templates/megaplan/img/prices/lady.png)").css("background-size","contain").css("background-repeat","no-repeat");
     });
